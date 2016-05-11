9f87ea8f5b3779e090b3b000eb973d17ad54eb5b
DoctorLar <doctorlar@gmail.com>
2016-05-10 21:12:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-apt-get update
-
 # install cups
 apt-get install cups cups-pdf python-cups --assume-yes
 usermod -a -G lpadmin pi
 lpadmin -x PDF
 /etc/init.d/cups restart
 
-# install a later version of gutenprint that has the Canon Selphy driver
+# install a later version of gutenprint 
 apt-get install gettext libcups2-dev automake libtool libtool-bin autopoint jade libcupsimage2-dev --assume-yes
 wget http://sourceforge.net/projects/gimp-print/files/gutenprint-5.2/5.2.11/gutenprint-5.2.11.tar.bz2
 
@@ -25,10 +23,3 @@ make install
 printf "\n\n"
 printf "Congratulations, your instagram printer has been configured. Next steps:\n"
 printf "1. Navigate to [your ip address]:693 in a web browser.\n"
-printf "2. Install your printer of choice (make sure sharing is Yes).\n"
-printf "3. Make the printer the default system printer.\n"
-
-printf "Note: if configuring using USB, you may need to run the following:\n"
-printf "1. lpadmin -p [your printer name] -o usb-unidir-default=true\n"
-printf "2. lpadmin -p [your printer name] -o usb-no-reattach-default=true\n"
-printf "Enjoy.\n"
